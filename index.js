730c607f28f40ba6d686da16c258fff83c36ec32
Bianca Giurgiu <giurgiu.bianca@yahoo.com>
2022-08-03 22:08:13 +0300

    secret Word + secretWord and revealLetter function

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 var word = ["M", "A", "R", "I", "A", "N", "E"];
+var hiddenWord = document.getElementById("word");
+
+function secretWord(word) {
+  for (var i = 0; i < word.length; i++) {
+    hiddenWord.innerHTML += `<div class="hiddenLetter" name="hidden-${word[i]}">${word[i]}</div>`;
+  }
+}
+
+secretWord(word);
+
+function revealLetter(correctLetter) {
+  var letter = document.getElementsByName(correctLetter);
+  for (var i = 0; i < letter.length; i++) {
+    letter[i].classList.remove("hiddenLetter");
+    letter[i].classList.add("revealedLetter");
+  }
+}
+
+// revealLetter("hidden-A");
