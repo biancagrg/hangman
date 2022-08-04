ff4bb78195a26c30c1a6fbb320fba83244e04727
Bianca Giurgiu <giurgiu.bianca@yahoo.com>
2022-08-04 22:47:35 +0300

    fetch words list once + new word when game over

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
+var wordList;
 var word;
 var hiddenWord = document.getElementById("word");
 var hangman = document.getElementById("counter");
 var counter = 0;
 
-function secretWord(word) {
+function secretWord() {
+  word = wordList.words[Math.floor(Math.random() * wordList.words.length)];
   for (var i = 0; i < word.length; i++) {
     hiddenWord.innerHTML += `<div class="hiddenLetter" name="hidden-${word[i]}">${word[i]}</div>`;
   }
 }
 
 function revealLetter(correctLetter) {
-  console.log("hier");
   var letter = document.getElementsByName(`hidden-${correctLetter}`);
-  console.log(letter[0]);
+
   for (var i = 0; i < letter.length; i++) {
-    console.log("muuuu");
     letter[i].classList.remove("hiddenLetter");
     letter[i].classList.add("revealedLetter");
   }
@@ -23,16 +23,14 @@ function revealLetter(correctLetter) {
 function initEvents() {
   document.getElementById("letters").addEventListener("click", function (e) {
     var letterId = e.target.getAttribute("id");
-    console.log("aici");
     if (checkLetter(letterId)) {
-      console.log(letterId);
       revealLetter(letterId);
     } else if (counter < 7) {
       counter++;
     } else {
       counter = 0;
       hiddenWord.innerHTML = "";
-      secretWord(word);
+      secretWord();
     }
     hangman.innerText = counter;
   });
@@ -40,7 +38,7 @@ function initEvents() {
 
 function checkLetter(id) {
   for (var i = 0; i < word.length; i++) {
-    if (word[i] == id) {
+    if (word[i] === id) {
       return true;
     }
   }
@@ -55,11 +53,8 @@ function loadWord() {
       return response.json();
     })
     .then(function (result) {
-      word =
-        result[0].words[
-          Math.floor(Math.random() * result[0].words.length)
-        ].split("");
-      secretWord(word);
+      wordList = result[0];
+      secretWord();
     });
 }
 
